6069fb3da86dabb41fc119fdee6aaf0ef46c7968
Team-Roxy-Hrd <168439280+Team-Roxy-Hrd@users.noreply.github.com>
2024-05-04 04:21:11 +0300

    Add files via upload

---
+++ java-script/menu.js
@@ -0,0 +1,10 @@
+function menu_op(){
+    document.getElementById('menu').style.display = 'flex'
+    document.getElementById('open').style.display = 'none'
+    document.getElementById('close').style.display = 'inline'
+};
+function menu_cl(){
+    document.getElementById('menu').style.display = 'none'
+    document.getElementById('open').style.display = 'inline'
+    document.getElementById('close').style.display = 'none'
+};
